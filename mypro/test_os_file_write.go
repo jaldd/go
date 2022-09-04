4af05f9a21060cd16869128d1315f7a19193a8eb
jaldd <806616508@qq.com>
2022-09-04 18:14:09 +0800

    io

--- mypro/test_os_file_write.go
+++ mypro/test_os_file_write.go
@@ -6,8 +6,12 @@ import (
 
 func write() {
 
-	f, _ := os.OpenFile("b.txt", os.O_RDWR, 0755)
+	f, _ := os.OpenFile("b.txt", os.O_RDWR|os.O_APPEND, 0755)
 	f.Write([]byte("helloworld"))
+	f.WriteString("hello world")
+	f.Close()
+	f, _ = os.OpenFile("b.txt", os.O_RDWR, 0755)
+	f.WriteAt([]byte("ldd 6"), 5)
 	f.Close()
 }
 
